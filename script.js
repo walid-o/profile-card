7dde1164a511babd067846a184d14e6aabd9afa6
walid-o <bentafatwalid2010@gmail.com>
2025-11-20 21:58:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 
+
 window.addEventListener("load", () => {
   document.getElementById("profileCard").classList.add("show");
 });
+ 
+
 
 function expandSnap(img) {
   img.classList.toggle("expanded");
 }
+
+
